0aa0cc2cf4da6e63f60e16c8c9cd396de90fd7c1
drmdgg <55467640+drmdgg@users.noreply.github.com>
2019-12-20 12:07:44 -0500

    Update References.java

--- src/main/java/drmdgg/blankmod/util/References.java
+++ src/main/java/drmdgg/blankmod/util/References.java
@@ -3,12 +3,12 @@ package drmdgg.blankmod.util;
 public class References {
 	public class Reference {
 
-		public static final String MOD_ID = "marijuanacraft";
-		public static final String NAME = "MarijuanaCraft";
+		public static final String MOD_ID = "blankmod";
+		public static final String NAME = "BlankMod";
 		public static final String VERSION = "0.1";
 		public static final String ACCEPTED_VERSIONS = "(1.14.4)";
-		public static final String CLIENT_PROXY_CLASS = "drmdgg.cropmod.proxy.ClientProxy";
-		public static final String COMMON_PROXY_CLASS = "drmdgg.cropmod.proxy.CommonProxy";
+		public static final String CLIENT_PROXY_CLASS = "drmdgg.blankmod.proxy.ClientProxy";
+		public static final String COMMON_PROXY_CLASS = "drmdgg.blankmod.proxy.CommonProxy";
 	}
 
 
